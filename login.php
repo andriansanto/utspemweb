76c251b44757cab1ce19a96fa5eaba5ea7677cbb
andriansanto <58908029+andriansanto@users.noreply.github.com>
2020-03-10 21:55:04 +0700

    css

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="login.css" rel="stylesheet" type="text/css" media="all" />
+  <link href="loginn.css" rel="stylesheet" type="text/css" media="all" />
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
