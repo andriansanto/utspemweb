0787d94328dcd22c9d672923eec1295d1f1ff799
andriansanto <58908029+andriansanto@users.noreply.github.com>
2020-03-10 22:21:08 +0700

    register

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link href="regis.css" rel="stylesheet" type="text/css" media="all" />
+  <link href="register.css" rel="stylesheet" type="text/css" media="all" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
@@ -87,6 +87,7 @@
                             </div>
                         </div>
                         <div class="form-row pt-20">
+                            
                             <label class="label label--block">You are agree to our term & police 
                                 <label >Check
                                     <input type="checkbox" name="exist">
